0e5fb5616174c8618979bcf7b1f9e19203605b8f
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-11-13 20:46:35 +0800

    feat(Zombie): 毒结算

--- pvzdll/MyZombie/MyZombie.hpp
+++ pvzdll/MyZombie/MyZombie.hpp
@@ -6,4 +6,7 @@ class MyZombie : public PVZ::Zombie
 public:
 	MyZombie(int idoraddress) : PVZ::Zombie(idoraddress) {};
 	MyZombie(const PVZ::Zombie& zombie) : PVZ::Zombie(zombie.GetBaseAddress()) {};
+
+	/// @brief 毒的层数
+	INT_PROPERTY(PoisonStack, __get_PoS, __set_PoS, 0x140);
 };
