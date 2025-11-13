cc27605a6a7a7871519d271ea84cbb16ad5e0b1a
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-11-13 20:46:35 +0800

    feat(Zombie): 毒结算

--- pvzdll/MyBoard.hpp
+++ pvzdll/MyBoard.hpp
@@ -8,6 +8,10 @@ class MyBoard : public PVZ::Board
 public:
 	MyBoard(int address) : PVZ::Board(address) {};
 	MyBoard(const PVZ::Board& board) : PVZ::Board(board.GetBaseAddress()) {};
+
+	/// @brief 毒伤结算倒计时
+	T_PROPERTY(uint8_t, PoisonCounter, __get_PoC, __set_PoC, 0x151);
+
 	/// @brief 在指定行右侧生成一只指定类型的僵尸
 	/// @param type 僵尸类型
 	/// @param row 行
